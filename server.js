9bc07894a1f327df1d76baf85aa5e009b0713541
Robson Ferreira de Fairas <robsonfdfarias@gmail.com>
2024-07-02 12:02:36 -0300

    adicionado o recurso para manter a conversa salva caso atualize a página. E caso fique sem internet, o que os outros tiverem conversado, aparecerá para o usuário quando a internet dele voltar. O recurso carregará as messagens apenas que quem fizer o primeiro acesso ou atualizar a página.

--- server.js
+++ server.js
@@ -1,22 +1,39 @@
+'use strict';
 const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
+const {readFile} = require('fs');
+const fs = require('fs');
+
+const shell = require('shelljs'); //para executar comandos shell
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-    // res.send('<h1>Hello world</h1>');
-    // console.log(__dirname)
     res.sendFile(join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
     console.log("a user connected. Id="+socket.id);
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    const keyNameChat = 'chat message';
+    const fileName = keyNameChat.replace(' ', '-')+'.txt';
+    console.log(fileName)
+    if(shell.test('-f', fileName)){
+        console.log('o arquivo existe');
+        let chama = 0;
+        if(chama<1){
+            readFile2(fileName, socket, keyNameChat)
+            chama++;
+        }
+    }else{
+        console.log('o arquivo NÃO existe');
+    }
+    socket.on(keyNameChat, (msg) => {
+        io.emit(keyNameChat, msg);
+        shell.echo([[msg.user, msg.msg]]).toEnd(fileName);
         console.log('User: '+msg.user)
         console.log('message: '+msg.msg)
     });
@@ -24,6 +41,24 @@ io.on('connection', (socket) => {
         console.log('user disconnected. Id='+socket.id)
     });
 })
+
+const readFile2 = (file, socket, keyNameChat) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if(err){
+            console.log(err)
+            return;
+        }
+        var ar = []
+        const array = data.split('\n');
+        array.forEach(function(linha, index){
+            console.log(linha)
+            ar.push(linha)
+        })
+        console.log('**********************************')
+        console.log(data)
+        socket.emit(keyNameChat, ar);
+    })
+}
 
 server.listen(3000, () => {
     console.log('server running at http://localhost:3000');
